49785d9172887cd0f714afb9ddcafaa157486e8c
Muhammad Anas <muhammadanasashfaq2006@gmail.com>
2025-09-09 10:22:33 +0500

    prctice 2

---
+++ Escapechar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//   escape character practice
+int main()
+{
+    printf("\n\t\"Escape character practice\"\n\n");
+    printf("\t1.\tItem One\n");
+    printf("\t2.\tItem Two\n");
+    printf("\t3.\tItem Three\n\n");
+    printf("\t\tEnd!\a\n");
+    return 0;
+}
